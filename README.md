b4692d15c52f5f1122d4b16fc9bd49290b0c34b7
John Menerick <john@syn.agency>
2019-01-20 20:13:31 +0000

    interesting project

---
+++ README.md
@@ -0,0 +1,55 @@
+> Records actions made in the AWS Management Console and outputs the equivalent CLI/SDK commands and CloudFormation/Terraform templates.
+
+
+![Screenshot](assets/screen1.png)
+
+:exclamation: **CAUTION:** This project is currently in beta stages. Some components may not work as expected.
+
+
+## Installation
+
+### Google Chrome
+
+You can download the extension from the [Chrome Web Store](https://chrome.google.com/webstore/detail/console-recorder/ganlhgooidfbijjidcpkeaohjnkeicba) or load the extension manually via chrome://extensions/.
+
+### Mozilla Firefox
+
+You can download the extension from [Firefox Add-ons](https://addons.mozilla.org/en-GB/firefox/addon/console-recorder/) or load the extension manually via about:addons. If loaded manually, replace the `manifest.json` file with the `firefoxManifest.json` file before loading.
+
+## Usage
+
+Click the orange _Console Recorder for AWS_ icon in the top-right corner of your browser window. If you do not see it, you may find it by clicking the three vertical dots and checking the top row. Once the popup is presented, click the **Start Recording** button.
+
+![Screenshot](assets/screen2.png)
+
+All supported actions will be recorded up until the point in which you click the icon again and select the **Stop Recording** button, at which point you will be presented with the dashboard for you to copy code from.
+
+The following outputs are currently supported:
+
+* Boto3 (Python) SDK
+* CloudFormation
+* HashiCorp Terraform
+* Troposphere
+* CDK (TypeScript)
+* AWS CLI (v1)
+* IAM Policy
+* JavaScript SDK
+* Go SDK (v1)
+
+## Settings
+
+In the Settings section, there are a number of options you can set. These options take effect as soon as you change them.
+
+### Block Mutable Requests
+
+This option will stop any mutable requests (most things that aren't a `Get*` or `List*` action) from executing in the console, whilst still logging the generated outputs in the dashboard.
+
+### Intercept Responses
+
+This option will record the response body from all AWS calls. Setting this allows CloudFormation, CDK, Troposphere and Terraform to correlate actions between resources. For example, CloudFormation will use the `!Ref` and `!GetAtt` intrinsic functions to reference resource identifiers within the template.
+
+:warning: **Note:** When Intercept Responses is set in Google Chrome, a notification telling you the extension is debugging the browser is shown for the duration of the recording for security purposes.
+
+## Security
+
+Network data from the AWS Management Console is recorded and information is extracted from the requests and responses. Recording data is kept entirely in memory or on local disk and is never sent over the internet or anywhere else. You should take care to remove any sensitive data (passwords etc.) when sharing your generated code/templates with others.
---
+++ main.js
@@ -0,0 +1,303 @@
+var boto3_editor, go_editor, cfn_editor, tf_editor, cli_editor, js_editor, cdkts_editor, iam_editor, troposphere_editor;
+
+window.onload = function(){
+    chrome.runtime.sendMessage(null, {
+        "action": "getBlockingStatus"
+    }, null, function(blocking_status){
+        if (blocking_status) {
+            document.getElementById('blocking').checked = true;
+        } else {
+            document.getElementById('blocking').checked = false;
+        }
+    });
+
+    chrome.runtime.sendMessage(null, {
+        "action": "getInterceptStatus"
+    }, null, function(intercept_status){
+        if (intercept_status) {
+            document.getElementById('intercept').checked = true;
+        } else {
+            document.getElementById('intercept').checked = false;
+        }
+    });
+
+    chrome.runtime.sendMessage(null, {
+        "action": "getTheme"
+    }, null, function(theme){
+        chrome.runtime.sendMessage(null, {
+            "action": "getCompiledOutputs"
+        }, null, function(response){
+            boto3_editor = CodeMirror.fromTextArea(document.getElementById('boto3'), {
+                lineNumbers: true,
+                lineWrapping: true,
+                mode: "python",
+                theme: theme,
+                indentUnit: 4
+            });
+            boto3_editor.getDoc().setValue(response['boto3']);
+    
+            go_editor = CodeMirror.fromTextArea(document.getElementById('go'), {
+                lineNumbers: true,
+                lineWrapping: true,
+                mode: "go",
+                theme: theme,
+                indentUnit: 4
+            });
+            go_editor.getDoc().setValue(response['go']);
+    
+            cfn_editor = CodeMirror.fromTextArea(document.getElementById('cfn'), {
+                lineNumbers: true,
+                lineWrapping: true,
+                mode: "yaml",
+                theme: theme,
+                indentUnit: 4
+            });
+            cfn_editor.getDoc().setValue(response['cfn']);
+    
+            tf_editor = CodeMirror.fromTextArea(document.getElementById('tf'), {
+                lineNumbers: true,
+                lineWrapping: true,
+                mode: "ruby",
+                theme: theme,
+                indentUnit: 4
+            });
+            tf_editor.getDoc().setValue(response['tf']);
+
+            cli_editor = CodeMirror.fromTextArea(document.getElementById('cli'), {
+                lineNumbers: true,
+                lineWrapping: true,
+                mode: "shell",
+                theme: theme,
+                indentUnit: 4
+            });
+            cli_editor.getDoc().setValue(response['cli']);
+    
+            js_editor = CodeMirror.fromTextArea(document.getElementById('js'), {
+                lineNumbers: true,
+                lineWrapping: true,
+                mode: "javascript",
+                theme: theme,
+                indentUnit: 4
+            });
+            js_editor.getDoc().setValue(response['js']);
+    
+            cdkts_editor = CodeMirror.fromTextArea(document.getElementById('cdkts'), {
+                lineNumbers: true,
+                lineWrapping: true,
+                mode: "javascript",
+                theme: theme,
+                indentUnit: 4
+            });
+            cdkts_editor.getDoc().setValue(response['cdkts']);
+    
+            iam_editor = CodeMirror.fromTextArea(document.getElementById('iam'), {
+                lineNumbers: true,
+                lineWrapping: true,
+                mode: "javascript",
+                theme: theme,
+                indentUnit: 4
+            });
+            iam_editor.getDoc().setValue(response['iam']);
+    
+            troposphere_editor = CodeMirror.fromTextArea(document.getElementById('troposphere'), {
+                lineNumbers: true,
+                lineWrapping: true,
+                mode: "python",
+                theme: theme,
+                indentUnit: 4
+            });
+            troposphere_editor.getDoc().setValue(response['troposphere']);
+            
+            setTimeout(function() {
+                boto3_editor.refresh();
+                go_editor.refresh();
+                cfn_editor.refresh();
+                tf_editor.refresh();
+                cli_editor.refresh();
+                js_editor.refresh();
+                cdkts_editor.refresh();
+                iam_editor.refresh();
+                troposphere_editor.refresh();
+            },1);
+
+            $('#theme').val(theme).trigger('change');
+
+            document.getElementById('theme').onchange = function(evt) {
+                if (evt.target.value) {
+                    chrome.runtime.sendMessage(null, {
+                        "action": "setTheme",
+                        "theme": evt.target.value
+                    });
+            
+                    boto3_editor.setOption("theme", evt.target.value);
+                    go_editor.setOption("theme", evt.target.value);
+                    cfn_editor.setOption("theme", evt.target.value);
+                    tf_editor.setOption("theme", evt.target.value);
+                    cli_editor.setOption("theme", evt.target.value);
+                    js_editor.setOption("theme", evt.target.value);
+                    cdkts_editor.setOption("theme", evt.target.value);
+                    iam_editor.setOption("theme", evt.target.value);
+                    troposphere_editor.setOption("theme", evt.target.value);
+                }
+            };
+        });
+    });
+
+    document.getElementById('blocking').onchange = function(evt) {
+        if (evt.target.checked) {
+            chrome.runtime.sendMessage(null, {
+                "action": "setBlockingOn"
+            });
+        } else {
+            chrome.runtime.sendMessage(null, {
+                "action": "setBlockingOff"
+            });
+        }
+    };
+    
+    document.getElementById('intercept').onchange = function(evt) {
+        if (evt.target.checked) {
+            chrome.runtime.sendMessage(null, {
+                "action": "setInterceptOn"
+            });
+        } else {
+            chrome.runtime.sendMessage(null, {
+                "action": "setInterceptOff"
+            });
+        }
+    };
+    
+    document.getElementById('cleardata_btn').onclick = function(evt) {
+        chrome.runtime.sendMessage(null, {
+            "action": "clearData"
+        }, null, function(result){
+            boto3_editor.getDoc().setValue("# No recorded actions yet");
+            go_editor.getDoc().setValue("// No recorded actions yet");
+            cfn_editor.getDoc().setValue("# No recorded actions yet");
+            tf_editor.getDoc().setValue("# No recorded actions yet");
+            cli_editor.getDoc().setValue("# No recorded actions yet");
+            js_editor.getDoc().setValue("// No recorded actions yet");
+            cdkts_editor.getDoc().setValue("// No recorded actions yet");
+            iam_editor.getDoc().setValue("// No recorded actions yet");
+            troposphere_editor.getDoc().setValue("# No recorded actions yet");
+
+            setTimeout(function() {
+                boto3_editor.refresh();
+                go_editor.refresh();
+                cfn_editor.refresh();
+                tf_editor.refresh();
+                cli_editor.refresh();
+                js_editor.refresh();
+                cdkts_editor.refresh();
+                iam_editor.refresh();
+                troposphere_editor.refresh();
+            },1);
+            
+            document.getElementById('cleardata_btn').innerHTML = "<span class=\"bold\">Cleared!</span>";
+            document.getElementById('cleardata_btn').setAttribute("class", "btn btn-default m-t-10");
+            document.getElementById('cleardata_btn').setAttribute("disabled", "disabled");
+        });
+    };
+};
+
+function resetMenu() {
+    document.getElementById('boto3_menubtn').setAttribute("class", "btn btn-default m-t-10");
+    document.getElementById('cli_menubtn').setAttribute("class", "btn btn-default m-t-10");
+    document.getElementById('go_menubtn').setAttribute("class", "btn btn-default m-t-10");
+    document.getElementById('cfn_menubtn').setAttribute("class", "btn btn-default m-t-10");
+    document.getElementById('tf_menubtn').setAttribute("class", "btn btn-default m-t-10");
+    document.getElementById('js_menubtn').setAttribute("class", "btn btn-default m-t-10");
+    document.getElementById('cdkts_menubtn').setAttribute("class", "btn btn-default m-t-10");
+    document.getElementById('iam_menubtn').setAttribute("class", "btn btn-default m-t-10");
+    document.getElementById('troposphere_menubtn').setAttribute("class", "btn btn-default m-t-10");
+    document.getElementById('settings_menubtn').setAttribute("class", "btn btn-default m-t-10");
+
+    document.getElementById('boto3_container').style = "display: none;";
+    document.getElementById('cli_container').style = "display: none;";
+    document.getElementById('go_container').style = "display: none;";
+    document.getElementById('cfn_container').style = "display: none;";
+    document.getElementById('tf_container').style = "display: none;";
+    document.getElementById('js_container').style = "display: none;";
+    document.getElementById('cdkts_container').style = "display: none;";
+    document.getElementById('iam_container').style = "display: none;";
+    document.getElementById('troposphere_container').style = "display: none;";
+    document.getElementById('settings_container').style = "display: none;";
+}
+
+document.getElementById('boto3_menubtn').onclick = function(evt) {
+    resetMenu();
+    document.getElementById('boto3_menubtn').setAttribute("class", "btn btn-primary m-t-10");
+    document.getElementById('boto3_container').style = "";
+
+    boto3_editor.refresh();
+};
+
+document.getElementById('cli_menubtn').onclick = function(evt) {
+    resetMenu();
+    document.getElementById('cli_menubtn').setAttribute("class", "btn btn-primary m-t-10");
+    document.getElementById('cli_container').style = "";
+
+    cli_editor.refresh();
+};
+
+document.getElementById('go_menubtn').onclick = function(evt) {
+    resetMenu();
+    document.getElementById('go_menubtn').setAttribute("class", "btn btn-primary m-t-10");
+    document.getElementById('go_container').style = "";
+
+    go_editor.refresh();
+};
+
+document.getElementById('cfn_menubtn').onclick = function(evt) {
+    resetMenu();
+    document.getElementById('cfn_menubtn').setAttribute("class", "btn btn-primary m-t-10");
+    document.getElementById('cfn_container').style = "";
+
+    cfn_editor.refresh();
+};
+
+document.getElementById('tf_menubtn').onclick = function(evt) {
+    resetMenu();
+    document.getElementById('tf_menubtn').setAttribute("class", "btn btn-primary m-t-10");
+    document.getElementById('tf_container').style = "";
+
+    tf_editor.refresh();
+};
+
+document.getElementById('js_menubtn').onclick = function(evt) {
+    resetMenu();
+    document.getElementById('js_menubtn').setAttribute("class", "btn btn-primary m-t-10");
+    document.getElementById('js_container').style = "";
+
+    js_editor.refresh();
+};
+
+document.getElementById('cdkts_menubtn').onclick = function(evt) {
+    resetMenu();
+    document.getElementById('cdkts_menubtn').setAttribute("class", "btn btn-primary m-t-10");
+    document.getElementById('cdkts_container').style = "";
+
+    cdkts_editor.refresh();
+};
+
+document.getElementById('iam_menubtn').onclick = function(evt) {
+    resetMenu();
+    document.getElementById('iam_menubtn').setAttribute("class", "btn btn-primary m-t-10");
+    document.getElementById('iam_container').style = "";
+
+    iam_editor.refresh();
+};
+
+document.getElementById('troposphere_menubtn').onclick = function(evt) {
+    resetMenu();
+    document.getElementById('troposphere_menubtn').setAttribute("class", "btn btn-primary m-t-10");
+    document.getElementById('troposphere_container').style = "";
+
+    troposphere_editor.refresh();
+};
+
+document.getElementById('settings_menubtn').onclick = function(evt) {
+    resetMenu();
+    document.getElementById('settings_menubtn').setAttribute("class", "btn btn-primary m-t-10");
+    document.getElementById('settings_container').style = "";
+};
